f569e1979d13b9983e5abbd6095681641a491762
Dussa, Vishnu Simha <vishnusimha.dussa@hidglobal.com>
2021-04-10 22:47:27 +0530

    Initial commit

---
+++ app/src/main/java/com/vishnu/navigationcomponent/fragments/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.vishnu.navigationcomponent.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.vishnu.navigationcomponent.R
+
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment2)
+        }
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/vishnu/navigationcomponent/fragments/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.vishnu.navigationcomponent.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.vishnu.navigationcomponent.R
+
+
+class SecondFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+        return view
+    }
+
+
+}
